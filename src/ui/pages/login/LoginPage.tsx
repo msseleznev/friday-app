9bfc46dce65c6a1fd1f21b8254a47c1217b4487d
msseleznev <mikestm8979@gmail.com>
2022-05-03 00:24:35 +0400

    Create a login-page structure

--- src/ui/pages/login/LoginPage.tsx
+++ src/ui/pages/login/LoginPage.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import s from "./Login.module.css"
+import testLogo from "../../../assets/images/TestLogo.png";
+import {SuperInputText} from "../../common/superInputText/SuperInputText"
+import {SuperButton} from "../../common/superButton/SuperButton";
+
 
 const LoginPage = () => {
 
+
     return (
-        <div>
-            LoginPage
+        <div className={s.loginBlock}>
+            <h2 className={s.title}>Sign In</h2>
+            <div className={s.loginContainer}>
+                <img src={testLogo} className={s.logo} alt={'logo'}/>
+                <form className={s.form}>
+                    <span>Email</span>
+                        <SuperInputText/>
+                    <span>Password</span>
+                        <SuperInputText/>
+                    <button className={s.button}>Forgot password</button>
+                    <SuperButton>
+                        Login
+                    </SuperButton>
+                </form>
+                <div>
+                    Don't have an account?
+                </div>
+                <button className={s.button}>Sing Up</button>
+            </div>
+
         </div>
     );
 };
