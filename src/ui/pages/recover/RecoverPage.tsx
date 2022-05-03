80d521d4a90fef2e23d42cbe33cf955ee277f5c9
msseleznev <mikestm8979@gmail.com>
2022-05-04 00:23:55 +0400

    Recover-page: Created jsx, css

--- src/ui/pages/recover/RecoverPage.tsx
+++ src/ui/pages/recover/RecoverPage.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import s from "./Recover.module.css";
+import testLogo from "../../../assets/images/TestLogo.png";
+import {SuperInputText} from "../../common/superInputText/SuperInputText";
+import {SuperButton} from "../../common/superButton/SuperButton";
 
 const RecoverPage = () => {
     return (
-        <div>
-            Recover password Page
+        <div className={s.recoverBlock}>
+            <h2 className={s.title}>Forgot password</h2>
+            <div className={s.recoverContainer}>
+                <img src={testLogo} className={s.logo} alt={'logo'}/>
+
+                <form className={s.form}>
+                    <h4>Forgot your password?</h4>
+                    <span>Email</span>
+                    <SuperInputText type='email'
+                        /*value={email}
+                        onChangeText={setEmail}
+                        error={emailError}*//>
+                    <p>Enter your email address and we will send you further instructions </p>
+
+                    <SuperButton>
+                        Send instructions
+                    </SuperButton>
+                </form>
+                <div>
+                    Did you remember your password?
+                </div>
+                <button className={s.button}>
+                    Try logging in
+                </button>
+            </div>
+
         </div>
     );
 };
