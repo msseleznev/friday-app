b429da2b2b11d0e465b5548562bf1794fcf4f7ed
msseleznev <mikestm8979@gmail.com>
2022-05-04 00:36:54 +0400

    Recover-page: Created a state for data

--- src/ui/pages/recover/RecoverPage.tsx
+++ src/ui/pages/recover/RecoverPage.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Recover.module.css";
 import testLogo from "../../../assets/images/TestLogo.png";
 import {SuperInputText} from "../../common/superInputText/SuperInputText";
 import {SuperButton} from "../../common/superButton/SuperButton";
+import {PATH} from "../../routes/RoutesApp";
+import {useNavigate} from "react-router-dom";
 
 const RecoverPage = () => {
+
+    const [email, setEmail] = useState<string>('')
+
+    const navigate = useNavigate()
+    const redirectToLogin = () => navigate(PATH.LOGIN)
+
+
     return (
         <div className={s.recoverBlock}>
             <h2 className={s.title}>Forgot password</h2>
@@ -15,19 +24,19 @@ const RecoverPage = () => {
                     <h4>Forgot your password?</h4>
                     <span>Email</span>
                     <SuperInputText type='email'
-                        /*value={email}
-                        onChangeText={setEmail}
-                        error={emailError}*//>
+                                    value={email}
+                                    onChangeText={setEmail}/>
                     <p>Enter your email address and we will send you further instructions </p>
 
-                    <SuperButton>
+                    <SuperButton disabled={email === ''}>
                         Send instructions
                     </SuperButton>
                 </form>
                 <div>
                     Did you remember your password?
                 </div>
-                <button className={s.button}>
+                <button className={s.button}
+                        onClick={redirectToLogin}>
                     Try logging in
                 </button>
             </div>
