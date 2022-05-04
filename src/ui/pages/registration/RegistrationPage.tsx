a2f3dee0b48603b42da15758e909f862eeedc946
IvanFestina <ivanfestina@gmail.com>
2022-05-04 19:35:56 +0300

    made some validation, two password and place for loading state

--- src/ui/pages/registration/RegistrationPage.tsx
+++ src/ui/pages/registration/RegistrationPage.tsx
@@ -5,27 +5,28 @@ import SuperInputText from "../../common/ivanSuperInputText/SuperInputText";
 import {SuperButton} from "../../common/superButton/SuperButton";
 import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {registerTC, setRedirectStatusAC} from "../../../bll/auth/registration/registration-reducer";
+import {registerTC, setRedirectToLoginAC} from "../../../bll/auth/registration/registration-reducer";
 import {AppStateType} from "../../../bll/store";
 import {PATH} from "../../routes/RoutesApp";
 
 export const RegistrationPage = (() => {
     const redirectToLogin = useSelector<AppStateType, boolean>(state => state.registration.redirectToLogin)
+    const error = useSelector<AppStateType, string>(state => state.registration.error)
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    // const [password2, setPassword2] = useState<string>('')
+    const [password2, setPassword2] = useState<string>('')
     const dispatch = useDispatch()
     let navigate = useNavigate();
 
 
     const onChangePasswordHandler = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
-    // const onChangePassword2Handler = (e: ChangeEvent<HTMLInputElement>) => setPassword2(e.currentTarget.value)
+    const onChangePassword2Handler = (e: ChangeEvent<HTMLInputElement>) => setPassword2(e.currentTarget.value)
     const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
-    const sendUserInfoOnclickButton = () => dispatch(registerTC(email, password))
+    const sendUserInfoOnclickButton = () => dispatch(registerTC(email, password, password2))
 
     if (redirectToLogin) {
         navigate(PATH.LOGIN)
-        dispatch(setRedirectStatusAC(false))
+        dispatch(setRedirectToLoginAC(false))
     }
 
     return (
@@ -42,15 +43,16 @@ export const RegistrationPage = (() => {
                     <div>
                         <SuperInputText value={password} onChange={onChangePasswordHandler}/>
                     </div>
-                    {/*<span>Repeat password</span>*/}
-                    {/*<div>*/}
-                    {/*    <SuperInputText value={password2} onChange={onChangePassword2Handler}/>*/}
-                    {/*</div>*/}
+                    <span>Repeat password</span>
+                    <div>
+                        <SuperInputText value={password2} onChange={onChangePassword2Handler}/>
+                    </div>
                     <div className={s.buttons}>
                         <SuperButton onClick={() => navigate(PATH.LOGIN)}>
                             To Login page
                         </SuperButton>
                         <SuperButton onClick={sendUserInfoOnclickButton}>Sign Up</SuperButton>
+                        <div>{error}</div>
                     </div>
                 </div>
             </div>
