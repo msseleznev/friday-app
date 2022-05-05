da260d25efefb3f59dee7251f74d3b1fac39e800
msseleznev <mikestm8979@gmail.com>
2022-05-05 21:50:57 +0400

    New-password: created a state for page data and display its text content

--- src/ui/pages/newPassword/NewPasswordPage.tsx
+++ src/ui/pages/newPassword/NewPasswordPage.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./NewPassword.module.css";
 import testLogo from "../../../assets/images/TestLogo.png";
 import {SuperInputText} from "../../common/superInputText/SuperInputText";
 import {SuperButton} from "../../common/superButton/SuperButton";
+import {useNavigate} from "react-router-dom";
+import {PATH} from "../../routes/RoutesApp";
+
 
 const NewPasswordPage = () => {
+
+    const [password, setPassword] = useState<string>('')
+    const [repPassword, setRepPassword] = useState<string>('')
+    const [isFetching, setIsFetching] = useState<boolean>(false)
+    const saveHandler = () => {
+        setIsFetching(true)
+    }
+
+    const navigate = useNavigate()
+    const redirectToLogin = () => navigate(PATH.LOGIN)
+
+
     return (
         <div className={s.newPasswordBlock}>
             <h2 className={s.title}>Create new password</h2>
             <div className={s.newPasswordContainer}>
                 <img src={testLogo} className={s.logo} alt={'logo'}/>
-                    <form className={s.form}>
-                        <p>Enter new password</p>
-                        <SuperInputText type='email'/>
-                        <SuperButton>
-                            Save new password
-                        </SuperButton>
-                    </form>
+                {isFetching
+                    ? <div className={s.form}>
+                        <h3>The new password has been successfully saved.</h3>
+                        <button className={s.button}
+                                onClick={redirectToLogin}>
+                            Login with new password
+                        </button>
+                    </div>
+                    :<form className={s.form}>
+                    <p>Enter new password</p>
+                    <SuperInputText type='password'
+                                    value={password}
+                                    onChangeText={setPassword}/>
+                    <p>Repeat new password</p>
+                    <SuperInputText type='password'
+                                    value={repPassword}
+                                    onChangeText={setRepPassword}/>
+                    <SuperButton disabled={password !== repPassword || password === ''}
+                                 onClick={saveHandler}>
+                        Save new password
+                    </SuperButton>
+                </form>}
+
             </div>
         </div>
     );
