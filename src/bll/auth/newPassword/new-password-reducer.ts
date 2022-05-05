2c5dc4ac5d96d462543f055cfddc552aa07b900b
msseleznev <mikestm8979@gmail.com>
2022-05-05 22:49:57 +0400

    New-password:created api request to the server and reducer

--- src/bll/auth/newPassword/new-password-reducer.ts
+++ src/bll/auth/newPassword/new-password-reducer.ts
@@ -1,3 +1,6 @@
+import {Dispatch} from "redux";
+import {ActionsType} from "../../store";
+import {authAPI, NewPasswordDataType} from "../../../api/api";
 
 export enum NEW_PASSWORD_ACTIONS_TYPE {
     SAVE_NEW_PASSWORD = 'SAVE_NEW_PASSWORD',
@@ -7,6 +10,7 @@ const initialState = {
     isFetching: false
 }
 type InitialStateType = typeof initialState
+export type NewPasswordActionsType = ReturnType<typeof saveNewPassword>
 
 export const newPasswordReducer = (state: InitialStateType = initialState, action: NewPasswordActionsType): InitialStateType => {
     switch (action.type) {
@@ -17,13 +21,22 @@ export const newPasswordReducer = (state: InitialStateType = initialState, actio
     }
 }
 
-
-
-export type NewPasswordActionsType = ReturnType<typeof saveNewPassword>
-
 export const saveNewPassword = (isFetching: boolean) => {
     return {
         type: NEW_PASSWORD_ACTIONS_TYPE.SAVE_NEW_PASSWORD,
         isFetching,
     }
+}
+//THUNKS
+export const saveNewPasswordTC = (data: NewPasswordDataType) => (dispatch: Dispatch<ActionsType>) => {
+    authAPI.setNewPassword(data)
+        .then((res) => {
+            console.log(res.data)
+            dispatch(saveNewPassword(true))
+        })
+        .catch((e) => {
+            console.log('Error: ', {...e})
+            const error = e.response ? e.response.data.error : (e.message + ', more details in the console')
+            alert(error)
+        })
 }
