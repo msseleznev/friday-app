3b9b2b36358e36c2fd30cb21e9f02b2b8c57d922
manchikooo <manchiko.man31@mail.ru>
2022-05-12 22:39:01 +0300

    add dependencies for useEffect in RegistrationPage.tsx. Made Search for CardsPage.tsx

--- src/ui/pages/cards/CardsPage.tsx
+++ src/ui/pages/cards/CardsPage.tsx
@@ -1,90 +1,109 @@
 import s from './CardsPage.module.css';
-import React, { useEffect, useState } from 'react';
-import { getCardsTC, sortCardsAC } from './cards-reducer';
-import { useAppDispatch, useAppSelector } from '../../../bll/hooks';
-import { Navigate, useParams } from 'react-router-dom';
-import { Card } from './Card/Card';
-import { PATH } from '../../routes/RoutesApp';
-import { SuperButton } from '../../common/superButton/SuperButton';
-import { SuperInputText } from '../../common/superInputText/SuperInputText';
+import React, {useEffect, useState} from 'react';
+import {getCardsTC, searchAnswer, searchQuestions, sortCardsAC} from './cards-reducer';
+import {useAppDispatch, useAppSelector} from '../../../bll/hooks';
+import {Navigate, useParams} from 'react-router-dom';
+import {Card} from './Card/Card';
+import {PATH} from '../../routes/RoutesApp';
+import {SuperButton} from '../../common/superButton/SuperButton';
+import {SuperInputText} from '../../common/superInputText/SuperInputText';
 import Modal from '../../common/Modal/Modal';
-import { sortPacks } from '../../../bll/packs/packs-reducer';
+import {searchPacks, sortPacks} from '../../../bll/packs/packs-reducer';
 
 export const CardsPage = () => {
-  const cards = useAppSelector(state => state.cards.cards);
-  const params = useAppSelector(state => state.cards.params);
-  const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
-  const dispatch = useAppDispatch();
+    const cards = useAppSelector(state => state.cards.cards);
+    const params = useAppSelector(state => state.cards.params);
+    const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
+    const dispatch = useAppDispatch();
 
-  const [sortParams, setSortParams] = useState<boolean>(false);
-  const [modalActive, setModalActive] = useState<boolean>(false);
+    const [sortParams, setSortParams] = useState<boolean>(false);
+    const [modalActive, setModalActive] = useState<boolean>(false);
 
+    const [questionValue, setQuestionValue] = useState<string>('')
+    const [answerValue, setAnswerValue] = useState<string>('')
 
-  const { packId } = useParams<{ packId: string }>();
+    const {packId} = useParams<{ packId: string }>();
 
-  useEffect(() => {
-    dispatch(getCardsTC(packId ? packId : ''));
-  }, [params]);
+    useEffect(() => {
+        dispatch(getCardsTC(packId ? packId : ''));
+    }, [params.cardQuestion, params.cardAnswer]);
 
-  const sortHandler = (e: any) => {
-  if (e.target.dataset) {
+    const sortHandler = (e: any) => {
+        if (e.target.dataset) {
             const trigger = e.currentTarget.dataset.sort
             dispatch(sortCardsAC({sortCards: `${Number(sortParams)}${trigger}`}))
             setSortParams(!sortParams)
         }
-  };
+    };
 
-  if (!isLoggedIn) {
-    return <Navigate to={PATH.LOGIN} />;
-  }
+    if (!isLoggedIn) {
+        return <Navigate to={PATH.LOGIN}/>;
+    }
 
-  return (
-    <div className={s.cardsBlock}>
+    const onEnterPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const trigger = e.currentTarget.dataset.input
+        if (e.code === 'Enter') {
+            if (trigger === 'searchQuestion') {
+                dispatch(searchQuestions({cardQuestion: questionValue}))
+            } else {
+                dispatch(searchAnswer({cardAnswer: answerValue}))
+            }
+        }
+    }
 
-      <div className={s.cardsContainer}>
-
-        <div className={s.contentBlock}>
-
-          <div className={s.searchBlock}>
-            <input/>
-            <input/>
-            <SuperButton onClick={() => setModalActive(true)}>+ Card</SuperButton>
-          </div>
-
-          <div className={s.tableBlock}>
-            <div className={s.tableHeader}>
-              <div className={s.question}
-                   onClick={sortHandler}
-                   data-sort='question'>Question
-              </div>
-              <div className={s.answer}
-                   onClick={sortHandler}
-                   data-sort='cardsCount'>Answer
-              </div>
-              <div className={s.updated}
-                   onClick={sortHandler}
-                   data-sort='updated'>Last Updated
-              </div>
-              <div className={s.actions}>Actions</div>
+    return (
+        <div className={s.cardsBlock}>
+            <div className={s.cardsContainer}>
+                <div className={s.contentBlock}>
+                    <div className={s.searchBlock}>
+                        <input
+                            placeholder={'Search by question'}
+                            value={questionValue}
+                            onKeyPress={onEnterPress}
+                            onChange={(e) => setQuestionValue(e.currentTarget.value)}
+                            data-input='searchQuestion'
+                            title="Press ENTER to search"
+                        />
+                        <input
+                            placeholder={'Search by answer'}
+                            value={answerValue}
+                            onKeyPress={onEnterPress}
+                            onChange={(e) => setAnswerValue(e.currentTarget.value)}
+                            data-input='searchAnswer'
+                            title="Press ENTER to search"
+                        />
+                        <SuperButton onClick={() => setModalActive(true)}>+ Card</SuperButton>
+                    </div>
+                    <div className={s.tableBlock}>
+                        <div className={s.tableHeader}>
+                            <div className={s.question}
+                                 onClick={sortHandler}
+                                 data-sort='question'>Question
+                            </div>
+                            <div className={s.answer}
+                                 onClick={sortHandler}
+                                 data-sort='cardsCount'>Answer
+                            </div>
+                            <div className={s.updated}
+                                 onClick={sortHandler}
+                                 data-sort='updated'>Last Updated
+                            </div>
+                            <div className={s.actions}>Actions</div>
+                        </div>
+                        {cards.map(card => <Card key={card._id} card={card}/>)}
+                    </div>
+                    <div className={s.paginationBlock}>Pagination</div>
+                </div>
             </div>
-            {cards.map(card => <Card key={card._id} card={card} />)}
-          </div>
-          <div className={s.paginationBlock}>Pagination</div>
-
+            <Modal active={modalActive} setActive={setModalActive}>
+                <h4>Add card</h4>
+                <p>Question</p>
+                <SuperInputText></SuperInputText>
+                <p>Answer</p>
+                <SuperInputText></SuperInputText>
+                <p>Attach image</p>
+                <SuperButton>Create card</SuperButton>
+            </Modal>
         </div>
-      </div>
-
-      <Modal active={modalActive} setActive={setModalActive}>
-        <h4>Add card</h4>
-        <p>Question</p>
-        <SuperInputText></SuperInputText>
-        <p>Answer</p>
-        <SuperInputText></SuperInputText>
-        <p>Attach image</p>
-        <SuperButton>Create card</SuperButton>
-      </Modal>
-
-    </div>
-
-  );
+    );
 };
--- src/ui/pages/cards/cards-reducer.ts
+++ src/ui/pages/cards/cards-reducer.ts
@@ -1,77 +1,82 @@
-import { cardsAPI, CardType } from './cardsApi';
-import { useAppDispatch, useAppSelector } from '../../../bll/hooks';
-import { setIsAppFetching } from '../../../bll/app/app-reducer';
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AppDispatch, AppStateType, AppThunk, NullableType } from '../../../bll/store';
-import { Dispatch } from 'redux';
-import { RootState } from '@reduxjs/toolkit/dist/query/core/apiState';
-import { packsAPI, PacksParamsType } from '../../../api/api';
-import { PacksActionsType } from '../../../bll/packs/packs-reducer';
+import {cardsAPI, CardType} from './cardsApi';
+import {useAppDispatch, useAppSelector} from '../../../bll/hooks';
+import {setIsAppFetching} from '../../../bll/app/app-reducer';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {AppDispatch, AppStateType, AppThunk, NullableType} from '../../../bll/store';
+import {Dispatch} from 'redux';
+import {RootState} from '@reduxjs/toolkit/dist/query/core/apiState';
+import {packsAPI, PacksParamsType} from '../../../api/api';
+import {PacksActionsType} from '../../../bll/packs/packs-reducer';
 
 const cardsInitialState = {
-  cards: [] as CardType[],
-  params: {
-    cardAnswer: '',
-    cardQuestion: '',
-    cardsPack_id: '',
-    min: 0,
-    max: 5,
-    sortCards: '0grade',
-    page: 1,
-    pageCount: 10,
-  } as CardsParamsType,
-  cardsTotalCount: 0,
-  packName: '',
+    cards: [] as CardType[],
+    params: {
+        cardAnswer: '',
+        cardQuestion: '',
+        cardsPack_id: '',
+        min: 0,
+        max: 5,
+        sortCards: '0grade',
+        page: 1,
+        pageCount: 10,
+    } as CardsParamsType,
+    cardsTotalCount: 0,
+    packName: '',
 };
-
+console.log(cardsInitialState)
 
 export const getCardsTC = createAsyncThunk<any, string, { state: AppStateType }>('cards/getCards',
-  async (packId: string, thunkAPI) => {
-    thunkAPI.dispatch(setIsAppFetching(true));
-    if (packId) {
-      thunkAPI.dispatch(setPackIdAC({ packId }));
-    }
-    try {
-      const params = thunkAPI.getState().cards.params
+    async (packId: string, thunkAPI) => {
+        thunkAPI.dispatch(setIsAppFetching(true));
+        if (packId) {
+            thunkAPI.dispatch(setPackIdAC({packId}));
+        }
+        try {
+            const params = thunkAPI.getState().cards.params
 
-      const data = await cardsAPI.getCards(params);
-      thunkAPI.dispatch(setCardsAC({ cards: data.cards }));
-      thunkAPI.dispatch(setCardsTotalCountAC({ cardsTotalCount: data.cardsTotalCount }));
-    } catch (e) {
-
-      console.warn(e);
-    } finally {
-      thunkAPI.dispatch(setIsAppFetching(false));
-    }
-  },
+            const data = await cardsAPI.getCards(params);
+            thunkAPI.dispatch(setCardsAC({cards: data.cards}));
+            thunkAPI.dispatch(setCardsTotalCountAC({cardsTotalCount: data.cardsTotalCount}));
+        } catch (e) {
+            console.warn(e);
+        } finally {
+            thunkAPI.dispatch(setIsAppFetching(false));
+        }
+    },
 );
 
 const slice = createSlice({
-  name: 'cards',
-  initialState: cardsInitialState,
-  reducers: {
-    setCardsAC(state, action: PayloadAction<{ cards: CardType[] }>) {
-      return { ...state, cards: action.payload.cards };
-    },
-    sortCardsAC(state, action: PayloadAction<{sortCards: string}>) {
-      return {...state, params: {...state.params, sortCards: action.payload.sortCards}}
-    },
-    setCardsTotalCountAC(state, action: PayloadAction<{ cardsTotalCount: NullableType<number> }>) {
+    name: 'cards',
+    initialState: cardsInitialState,
+    reducers: {
+        setCardsAC(state, action: PayloadAction<{ cards: CardType[] }>) {
+            return {...state, cards: action.payload.cards};
+        },
+        sortCardsAC(state, action: PayloadAction<{ sortCards: string }>) {
+            return {...state, params: {...state.params, sortCards: action.payload.sortCards}}
+        },
+        searchQuestions(state, action: PayloadAction<{cardQuestion: string}>){
+            return {...state, params: {...state.params, cardQuestion: action.payload.cardQuestion}}
+        },
+        searchAnswer(state, action: PayloadAction<{cardAnswer: string}>){
+            return {...state, params: {...state.params, cardAnswer: action.payload.cardAnswer}}
+        },
+        setCardsTotalCountAC(state, action: PayloadAction<{ cardsTotalCount: NullableType<number> }>) {
 
+        },
+        setPackIdAC(state, action: PayloadAction<{ packId: string }>) {
+            return {
+                ...state,
+                params: {...state.params, cardsPack_id: action.payload.packId},
+            };
+        },
     },
-    setPackIdAC(state, action: PayloadAction<{ packId: string }>) {
-      return {
-        ...state,
-        params: { ...state.params, cardsPack_id: action.payload.packId },
-      };
-    },
-  },
 });
 
 export const cardsReducer = slice.reducer;
 
 //ACTION CREATORS
-export const { setCardsAC, setCardsTotalCountAC, setPackIdAC, sortCardsAC } = slice.actions;
+export const {setCardsAC, setCardsTotalCountAC, setPackIdAC, sortCardsAC, searchQuestions, searchAnswer} = slice.actions;
 
 
 //thunks
@@ -93,40 +98,40 @@ export const { setCardsAC, setCardsTotalCountAC, setPackIdAC, sortCardsAC } = sl
 
 export const addCardTC = (cardsPack_id: string, question: string, answer: string) => async (dispatch: AppDispatch) => {
 
-  dispatch(setIsAppFetching(true));
-  try {
-    // await cardsAPI.addCard(card);
+    dispatch(setIsAppFetching(true));
+    try {
+        // await cardsAPI.addCard(card);
 
-  } catch (e) {
+    } catch (e) {
 
-  } finally {
-    dispatch(setIsAppFetching(false));
-  }
+    } finally {
+        dispatch(setIsAppFetching(false));
+    }
 };
 
 export const updateCardTC = (_id: string, question: string, answer: string) => async (dispatch: AppDispatch) => {
 
-  dispatch(setIsAppFetching(true));
-  try {
-    // await cardsAPI.updateCard(card);
+    dispatch(setIsAppFetching(true));
+    try {
+        // await cardsAPI.updateCard(card);
 
-  } catch (e) {
+    } catch (e) {
 
-  } finally {
-    dispatch(setIsAppFetching(false));
-  }
+    } finally {
+        dispatch(setIsAppFetching(false));
+    }
 };
 
 //types
 export type CardsInitialStateType = typeof cardsInitialState
 export type CardsParamsType = {
-  cardAnswer?: string
-  cardQuestion?: string
-  cardsPack_id?: string
-  min?: number,
-  max?: number,
-  sortCards?: string
-  page?: number
-  pageCount?: number
+    cardAnswer?: string
+    cardQuestion?: string
+    cardsPack_id?: string
+    min?: number,
+    max?: number,
+    sortCards?: string
+    page?: number
+    pageCount?: number
 }
 // export type CardsSortFieldsType = 'answer' | 'question' | 'updated' | 'grade'
