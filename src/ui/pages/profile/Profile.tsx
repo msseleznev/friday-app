e072ce9e21550c7d64cb8c7b854a28d78312f91b
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-05-12 13:13:33 +0300

    profile: modal for change photo (part 1)

--- src/ui/pages/profile/Profile.tsx
+++ src/ui/pages/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import style from './Profile.module.scss'
 import {Navigate} from 'react-router-dom';
 import {PATH} from '../../routes/RoutesApp';
@@ -10,6 +10,7 @@ import {updateProfileUserData} from '../../../bll/profile/profile-reducer';
 import {Preloader} from '../../common/Preloader/Preloader';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCamera} from '@fortawesome/free-solid-svg-icons/faCamera';
+import Modal from '../../common/Modal/Modal';
 
 export const Profile = () => {
     const {name, avatar} = useAppSelector(state => state.profile.user);
@@ -17,12 +18,22 @@ export const Profile = () => {
     const isProfileFetching = useAppSelector<boolean>(state => state.profile.isProfileFetching);
     const [newNickname, setNewNickname] = useState(name);
     const [newAvatar, setNewAvatar] = useState('https://icon-library.com/images/icon-avatar/icon-avatar-19.jpg');
+    const [howUploadPhoto, setHowUploadPhoto] = useState<'asFile' | 'asURL'>('asURL');
+    const [modalActive, setModalActive] = useState<boolean>(false)
     const dispatch = useAppDispatch();
     const updateNickName = () => {
         if (newNickname.trim() !== name) {
             dispatch(updateProfileUserData(newNickname))
         }
     };
+
+    // на будущее!!!
+    // const onChangeAvatarPhoto = (e:ChangeEvent<HTMLInputElement>) => {
+    //     if (e.target.files && e.target.files.length) {
+    //         dispatch(updateProfileUserData(e.target.files[0]);
+    //     }
+    // };
+
     if (!isLoggedIn) {
         return <Navigate to={PATH.LOGIN}/>
     }
@@ -33,7 +44,8 @@ export const Profile = () => {
                     <div className={style.avatar}>
                         <img src={avatar ? avatar : defaultAvatar} alt="Avatar"/>
                     </div>
-                    <div className={style.changeAvatar}>
+                    <div className={style.changeAvatar}
+                         onClick={() => setModalActive(true)}>
                         <FontAwesomeIcon icon={faCamera}/>
                     </div>
                 </div>
@@ -49,6 +61,10 @@ export const Profile = () => {
                     Front-end developer
             </span>
             </div>
+            <Modal active={modalActive} setActive={setModalActive}>
+                <input type="file"/>
+                <input type="text"/>
+            </Modal>
         </div>
     );
 };
